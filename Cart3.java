c3bf74049470545ba741a392479e2d12905d16e5
mellis49 <123321925+mellis49@users.noreply.github.com>
2023-02-26 18:11:20 -0600

    Add files via upload

---
+++ Cart3.java
@@ -0,0 +1,50 @@
+package Milestone3;
+import java.util.*;
+public class Cart3 {
+	int purchaseSwords;
+	int purchaseShurikans;
+	int purchaseHelmets;
+	int purchaseBoots;
+	int purchaseHealthPotions;
+	
+	public int getpurchaseSwords() {
+	return purchaseSwords;
+	}
+	public void setpurchaseSwords(int purchaseSwords) {
+		this.purchaseSwords = purchaseSwords;
+		
+	}
+	
+	public int getpurchaseShurikans() {
+		return purchaseShurikans;
+		}
+	public void setpurchaseShurikans(int purchaseShurikans) {
+		this.purchaseShurikans = purchaseShurikans;
+		
+	}
+	
+	public int getpurchaseHelmets() {
+		return purchaseHelmets;
+		}
+	public void setpurchaseHelmets(int purchaseHelmets) {
+		this.purchaseHelmets = purchaseHelmets;
+			
+	}
+	
+	public int getpurchaseBoots() {
+		return purchaseBoots;
+		}
+	public void setpurchaseBoots(int purchaseBoots) {
+		this.purchaseBoots = purchaseBoots;
+			
+		}
+	
+	public int getpurchaseHealthPotions() {
+		return purchaseHealthPotions;
+		}
+	public void setpurchaseHealthPotions(int purchaseHealthPotions) {
+		this.purchaseHealthPotions = purchaseHealthPotions;
+			
+		}
+
+}
